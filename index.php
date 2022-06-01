7d7a1f64b54ac06e741c512af217e909cb0e4584
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-01 17:54:42 -0500

    igual que el commit anterior

--- index.php
+++ index.php
@@ -14,17 +14,7 @@
 
 <body>
     <!-- Nav -->
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Productos</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div>
-                <a class="btn btn-outline-success" href="iniciar_sesion.php">Iniciar Sesión</a>
-            </div>
-        </div>
-    </nav>
+    <?php include("nav.php"); ?>
 
     <!-- Agregar productos -->
 
